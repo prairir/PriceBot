c2bf663f4e375883163d622ec9d7a93d68f8e8ab
prairir <prairie421@gmail.com>
2018-11-23 04:04:36 -0500

    adding arguement parsing for all options

--- pb.py
+++ pb.py
@@ -1,7 +1,9 @@
 import discord
+from amzsear import AmzSear
 import asyncio
 import re
 from time import sleep
+import argparse
 
 client = discord.Client()
 monitors = []
@@ -10,6 +12,15 @@ token = open('token.txt', 'r')
 token = token.read()
 
 url_reg=r"(?i)amazon\.(ca|com|jp|au|uk|br|de|es|fr|in|it|mx|nl|sg)/([^\s]+)"
+
+#search parse
+search_parse = argparse.ArgumentParser()
+search_parse = add_argument('--region', '-r', help='choose for regions \n |AU| Australia |\n |BR| Brazil    |\n |CA| Canada    |\n |CN| China     |\n |De| Germany   |\n |ES| Spain     |\n |FR| France    |\n |IN| India     |\n |IT| Italy     |\n |JP| Japan     |\n |MX| Mexico    |\n |NL| Holland   |\n |SG| Singapore |\n |UK| England   |\n |US| America   |\n')
+
+#url parse setup
+url_parse = argparse.ArgumentParser(prog="!addurl", "%(prog)s [url]")
+url_parse.add_argument('url',)
+
 
 @client.event
 async def on_ready():
@@ -22,10 +33,11 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('!help'):
-        await client.send_message(message.channel, '''```!help - displays this message \n!addurl - watches the url given \n!add - watches the first amazon item it finds \n!search - searches 10 items```''')
+        await client.send_message(message.channel, '''```!help - displays this message \n!addurl - watches the url given \n!add - watches the first amazon item it finds \n!search - searches the first page \n \n \nfor more information type in any command and then --help for example: !add --help```''')
     
     if message.content.startswith('!addurl'):
-        url = message.split()[1]
+
+
 
 client.run(token)
 token.close()
