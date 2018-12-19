1b895248894ccc8d6492292dc959a3721389f827
prairir <prairie421@gmail.com>
2018-12-18 20:48:29 -0500

    removed some crap

--- pb.py
+++ pb.py
@@ -1,25 +1,26 @@
 import discord
+import argparse
 from amzsear import AmzSear
 import asyncio
 import re
+from args import ArgumentParser
 from time import sleep
-import argparse
 
 client = discord.Client()
 monitors = []
 checkings = []
-token = open('token.txt', 'r')
+token = open('token.key', 'r')
 token = token.read()
 
 url_reg=r"(?i)amazon\.(ca|com|jp|au|uk|br|de|es|fr|in|it|mx|nl|sg)/([^\s]+)"
 
 #search parse
 search_parse = argparse.ArgumentParser()
-search_parse = add_argument('--region', '-r', help='choose for regions \n |AU| Australia |\n |BR| Brazil    |\n |CA| Canada    |\n |CN| China     |\n |De| Germany   |\n |ES| Spain     |\n |FR| France    |\n |IN| India     |\n |IT| Italy     |\n |JP| Japan     |\n |MX| Mexico    |\n |NL| Holland   |\n |SG| Singapore |\n |UK| England   |\n |US| America   |\n')
+search_parse.add_argument('--region', '-r', help='choose for regions \n |AU| Australia |\n |BR| Brazil    |\n |CA| Canada    |\n |CN| China     |\n |De| Germany   |\n |ES| Spain     |\n |FR| France    |\n |IN| India     |\n |IT| Italy     |\n |JP| Japan     |\n |MX| Mexico    |\n |NL| Holland   |\n |SG| Singapore |\n |UK| England   |\n |US| America   |\n')
 
 #url parse setup
-url_parse = argparse.ArgumentParser(prog="!addurl", "%(prog)s [url]")
-url_parse.add_argument('url',)
+url_parse = ArgumentParser(prog="!addurl")
+url_parse.add_argument('url')
 
 
 @client.event
@@ -36,7 +37,10 @@ async def on_message(message):
         await client.send_message(message.channel, '''```!help - displays this message \n!addurl - watches the url given \n!add - watches the first amazon item it finds \n!search - searches the first page \n \n \nfor more information type in any command and then --help for example: !add --help```''')
     
     if message.content.startswith('!addurl'):
-
+        try:
+            url_parse.parse_args(message.content[7:].split())
+        except argparse.ArgumentError or exc:
+            client.send_message(message.channel,'''```{}```'''.format(exc.message, '\n', exc.argument))
 
 
 client.run(token)
